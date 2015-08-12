64119c1fb79e841b548baea8cabcd5c7e82362fa
J Galbraith <thedirtycolumbia@gmail.com>
2015-08-11 18:51:28 -0700

    initial commit, scrapes top recipients data

---
+++ scraper.R
@@ -0,0 +1,33 @@
+library("rvest")
+library("magrittr")
+
+url <- "http://farm.ewg.org/"
+s <- html_session("http://farm.ewg.org/top_recips.php?fips=41000&progcode=totalfarm&page=0")
+
+scrape_tables <- function(session, filename) {
+  print(session)
+  
+  t <- session %>%
+    html_node("#top_recip_table") %>%
+    html_table()
+  
+  write.table(t,
+              file = filename,
+              col.names = FALSE,
+              row.names = FALSE,
+              append = TRUE,
+              sep = ",")
+  
+  next_page <- session %>%
+    html_nodes("strong a") %>%
+    html_attr("href") %>%
+    XML::getRelativeURL(url)
+  s <- html_session(next_page)
+  
+  scrape_tables(s, filename)
+}
+
+scrape_tables(s, "top_recipientz.csv")
+
+# loading the data back into R looks like:
+# df <- read.csv("filename.csv", header = F, col.names = c("rank", "recipient", "location", "subtotal"))
